cd1cb6bbf13ff12b286cd685b0e187ff8d90a965
Marco <71723426+marcogonzalez99@users.noreply.github.com>
2022-09-28 13:30:50 -0600

    Added Lesson 6

---
+++ Lessons/Lesson_6/making_pizza.py
@@ -0,0 +1,11 @@
+from pizza import make_pizza as mp
+# Can also do - import pizza as p
+# p.make_pizza()
+
+mp(16,'pepperoni')
+mp(12,'mushrooms','green peppers','extra cheese')
+
+# Other Methods
+
+# From module_name import function_name
+# From module_name import function_0, function_1, function_2

2b0627cb96c88a884fc57e2d5305b327461bf110
marcogonzalez99 <Mgonz781@mtroyal.ca>
2022-09-30 23:54:06 -0600

    Added New Game

---
+++ Space_Invaders/space_invaders.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+from player import Player
+
+
+class Game:
+    def __init__(self):
+        player_sprite = Player(
+            (screen_width/2, screen_height), screen_width, 5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+    def run(self):
+        self.player.update()
+
+        self.player.sprite.lasers.draw(screen)
+        self.player.draw(screen)
+
+
+if __name__ == '__main__':
+    # General Setup
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    # Game Screen
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Space Invaders")
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30, 30, 30))
+        game.run()
+        pygame.display.flip()
+        clock.tick(75)

b00a75d1dd72bb04c8791d5776c03e4c74d5051d
Marco <71723426+marcogonzalez99@users.noreply.github.com>
2022-10-03 11:10:00 -0600

    final pong changes

--- Lessons/9-Pong.py
+++ Lessons/9-Pong.py
@@ -2,55 +2,6 @@
 import pygame
 import sys
 import random
-
-def ball_animation():
-    global ball_speed_x,ball_speed_y,player_score,opponent_score
-    
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y 
-    
-    # Ball Collision
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-        
-    if ball.left <= 0:
-        player_score += 1
-        score_sound.play()
-        ball_restart()
-    if ball.right >= screen_width:
-        opponent_score += 1
-        score_sound.play()
-        ball_restart()
-    
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        pong_sound.play()
-        ball_speed_x *= -1
-        
-
-def player_animation():
-    player.y += player_speed
-    #Player Collision
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= screen_height:
-        player.bottom = screen_height    
-    
-def opponent_ai():
-    if opponent.top < ball.y:  # This adjustment is for how strong the AI will be
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:  # This adjustment is for how strong the AI will be
-        opponent.bottom -= opponent_speed
-    if opponent.top <= 0:
-        opponent.top = 0 + 5
-    if opponent.bottom >= screen_height:
-        opponent.bottom = screen_height - 5
-        
-def ball_restart():
-    global ball_speed_x,ball_speed_y
-    ball_speed_x *= random.choice((-1,1))
-    ball_speed_y *= random.choice((-1,1))
-    ball.center = (screen_width/2, screen_height/2)
-    
     
 #General Setup
 pygame.init()
@@ -87,38 +38,87 @@ game_font = pygame.font.Font(None,44)
 pong_sound = pygame.mixer.Sound("pong.ogg")
 score_sound = pygame.mixer.Sound("wall.ogg")
 
-# Game Loop
-while True:
+
+def ball_animation():
+    global ball_speed_x,ball_speed_y,player_score,opponent_score
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()    
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y 
+    
+    # Ball Collision
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                player_speed += 9
-            if event.key == pygame.K_UP:
-                player_speed -= 9
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                player_speed -= 9
-            if event.key == pygame.K_UP:
-                player_speed += 9
-                
-    ball_animation()
-    player_animation()
-    opponent_ai()
+    if ball.left <= 0:
+        player_score += 1
+        score_sound.play()
+        ball_restart()
+    if ball.right >= screen_width:
+        opponent_score += 1
+        score_sound.play()
+        ball_restart()
     
-    screen.fill(bg_color)        
-    pygame.draw.ellipse(screen, light_grey, ball)
-    pygame.draw.rect(screen, light_grey, opponent)
-    pygame.draw.rect(screen, light_grey, player)
-    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        pong_sound.play()
+        ball_speed_x *= -1   
+
+def player_animation():
+    player.y += player_speed
+    #Player Collision
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height    
     
-    # Surface for scoreboard
-    player_test = game_font.render(f"{opponent_score} : {player_score}",False,('white'))
-    screen.blit(player_test,(screen_width/2 - 30,screen_height/2))
+def opponent_ai():
+    if opponent.top < ball.y:  # This adjustment is for how strong the AI will be
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:  # This adjustment is for how strong the AI will be
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0 + 5
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height - 5
+        
+def ball_restart():
+    global ball_speed_x,ball_speed_y
+    ball_speed_x *= random.choice((-1,1))
+    ball_speed_y *= random.choice((-1,1))
+    ball.center = (screen_width/2, screen_height/2)
     
-    pygame.display.flip()
-    clock.tick(60)
+if __name__ == "__main__":
+    # Game Loop
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()    
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player_speed += 9
+                if event.key == pygame.K_UP:
+                    player_speed -= 9
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    player_speed -= 9
+                if event.key == pygame.K_UP:
+                    player_speed += 9
+                    
+        ball_animation()
+        player_animation()
+        opponent_ai()
+        
+        screen.fill(bg_color)        
+        pygame.draw.ellipse(screen, light_grey, ball)
+        pygame.draw.rect(screen, light_grey, opponent)
+        pygame.draw.rect(screen, light_grey, player)
+        pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
+        
+        # Surface for scoreboard
+        player_test = game_font.render(f"{opponent_score} : {player_score}",False,('white'))
+        screen.blit(player_test,(screen_width/2 - 30,screen_height/2))
+        
+        pygame.display.flip()
+        clock.tick(60)  

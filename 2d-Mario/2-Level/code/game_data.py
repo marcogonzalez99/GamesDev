3287768c83689c52316e5584d900c051e44506ed
Marco <mgonz781@mtroyal.ca>
2022-10-07 15:33:41 -0600

    Added a 7th level

--- 2d-Mario/2-Level/code/game_data.py
+++ 2d-Mario/2-Level/code/game_data.py
@@ -79,6 +79,19 @@ level_5 = {
     'node_pos': (1050,400),
     'unlock': 5,
     'graphics': '../graphics/overworld/5'}
+level_6 = {
+    'terrain': '../levels/0/level_0_Terrain.csv',
+    'coins': '../levels/0/level_0_Coins.csv',
+    'fg palms': '../levels/0/level_0_fg_palms.csv',
+    'bg palms': '../levels/0/level_0_bg_palms.csv',
+    'crates': '../levels/0/level_0_crates.csv',
+    'enemies': '../levels/0/level_0_enemies.csv',
+    'constraints': '../levels/0/level_0_constraints.csv',
+    'player': '../levels/0/level_0_player.csv',
+    'grass': '../levels/0/level_0_grass.csv',
+    'node_pos': (1200,150),
+    'unlock': 5,
+    'graphics': '../graphics/overworld/0'}
 
 levels = {
     0: level_0,
@@ -87,4 +100,5 @@ levels = {
     3: level_3,
     4: level_4,
     5: level_5,
+    6: level_6,
 }
--- 2d-Mario/2-Level/code/main.py
+++ 2d-Mario/2-Level/code/main.py
@@ -8,7 +8,7 @@ from ui import UI
 class Game:
     def __init__(self):
         # Game Attributes
-        self.max_level = 0
+        self.max_level = 6
         # PLayer Attributes
         self.current_health = 100
         self.max_health = 100

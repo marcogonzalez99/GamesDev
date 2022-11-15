8fcc58669fffc94302aa2300c5863868540acd7c
Marco <mgonz781@mtroyal.ca>
2022-11-15 15:25:29 -0700

    fixed main to only run

--- Pirate_Cove/code/settings.py
+++ Pirate_Cove/code/settings.py
@@ -1,3 +1,4 @@
+import pygame
 # How many tiles high the screen will be
 vertical_tile_number = 16
 # How large each tile is
@@ -5,3 +6,5 @@ tile_size = 64
 # Get the Screen Height and Width
 screen_height = vertical_tile_number * tile_size
 screen_width = 1920
+
+screen = pygame.display.set_mode((screen_width, screen_height))

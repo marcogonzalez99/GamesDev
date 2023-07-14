3a42cde9e563e1bb1c8ae1a57785dbfb4f860592
marcogonzalez99 <Mgonz781@mtroyal.ca>
2023-07-13 21:48:23 -0600

    Added collision to the ball and player

--- Breakout/code/sprites.py
+++ Breakout/code/sprites.py
@@ -12,6 +12,8 @@ class Player(pygame.sprite.Sprite):
         
         # Position
         self.rect = self.image.get_rect(midbottom = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 20))
+        # Create old rect
+        self.old_rect = self.rect.copy()
         self.direction = pygame.math.Vector2()
         self.speed = 300
         self.pos = pygame.math.Vector2(self.rect.topleft)
@@ -34,6 +36,7 @@ class Player(pygame.sprite.Sprite):
             self.pos.x = self.rect.x  
             
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         
         # Checks for screen constraint
@@ -54,6 +57,7 @@ class Ball(pygame.sprite.Sprite):
         
         # Position Setup
         self.rect = self.image.get_rect(midbottom = player.rect.midtop)
+        self.old_rect = self.rect.copy()
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(choice((1,-1)),-1)
         self.speed = 400
@@ -63,13 +67,56 @@ class Ball(pygame.sprite.Sprite):
     
     def window_collision(self,direction):
         if direction == 'horizontal':
-            pass
+            if self.rect.left < 0:
+                self.rect.left = 0
+                self.pos.x = self.rect.x
+                self.direction.x *= -1
+                
+            if self.rect.right > WINDOW_WIDTH:
+                self.rect.right = WINDOW_WIDTH
+                self.pos.x = self.rect.x
+                self.direction.x *= -1
         if direction == 'vertical':
-            pass
+            if self.rect.top < 0:
+                self.rect.top = 0
+                self.pos.y = self.rect.y
+                self.direction.y *= -1
+                
+            if self.rect.bottom > WINDOW_HEIGHT:
+                self.active = False
+                self.direction.y = -1
         
     
-    def collision(self):
-        pass
+    def collision(self,direction):
+        # Find Overlapping Objects
+        overlap_sprites = []
+        if self.rect.colliderect(self.player.rect):
+            overlap_sprites.append(self.player)
+            
+        if overlap_sprites:
+            if direction == 'horizontal':
+                for sprite in overlap_sprites:
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left - 1
+                        self.pos.x = self.rect.x
+                        self.direction.x *= -1
+ 
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right + 1
+                        self.pos.x = self.rect.x
+                        self.direction.x *= -1
+                        
+            if direction == 'vertical':
+                for sprite in overlap_sprites:
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top - 1
+                        self.pos.y = self.rect.y
+                        self.direction.y *= -1
+ 
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom + 1
+                        self.pos.y = self.rect.y
+                        self.direction.y *= -1
         
     
     def update(self,dt):
@@ -77,12 +124,19 @@ class Ball(pygame.sprite.Sprite):
             if self.direction.magnitude() != 0:
                 self.direction = self.direction.normalize()
             
+            # Create old rect
+            self.old_rect = self.rect.copy()
+            
+            # Horizontal Movement + Collision
             self.pos.x += self.direction.x * self.speed * dt
             self.rect.x = round(self.pos.x)
+            self.collision('horizontal')
             self.window_collision('horizontal')
             
+            # Vertical Movement + Collision
             self.pos.y += self.direction.y * self.speed * dt
             self.rect.y = round(self.pos.y)
+            self.collision('vertical')
             self.window_collision('vertical')
         else:
             self.rect.midbottom = self.player.rect.midtop
